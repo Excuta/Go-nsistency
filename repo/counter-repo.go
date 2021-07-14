d5e58e431b810cdeab68077ccec50b0fbfd467b2
yahia <ymr_15@live.com>
2021-07-14 23:05:51 +0200

    QOF and code enhancements

--- repo/counter-repo.go
+++ repo/counter-repo.go
@@ -10,34 +10,36 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
-var client = redis.NewClient(&redis.Options{
-	Addr:     "192.168.1.205:6379",
+const BASE_URL = "192.168.1.108"
+
+var redisClient = redis.NewClient(&redis.Options{
+	Addr:     fmt.Sprintf("%v:6379", BASE_URL),
 	Password: "",
 	DB:       0,
 })
 
-const counterKey = "counter"
-const expirySeconds = 30 * time.Second
+var pgClient, pgerr = pgx.Connect(context.Background(), fmt.Sprintf("postgres://yahia:2472BvZFgUNrof@%v:5432/counter_db", BASE_URL))
 
-var pgClient, pgerr = pgx.Connect(context.Background(), "postgres://yahia:2472BvZFgUNrof@192.168.1.205:5432/counter_db")
+const REDIS_COUNTER_KEY = "counter"
+const REDIS_COUNTER_EXPIRY_MILLIS = 30 * time.Second
 
 func init() {
 	pgClient.Exec(context.Background(), "CREATE TABLE IF NOT EXISTS public.counters(id uuid NOT NULL, value bigint NOT NULL, CONSTRAINT \"unique id\" PRIMARY KEY (id), CONSTRAINT \"value non negative\" CHECK (value >= 0))")
 	var count = 0
 	pgClient.QueryRow(context.Background(), "SELECT COUNT(*) FROM public.counters;").Scan(&count)
 	if count == 0 {
-		pgClient.Exec(context.Background(), "INSERT INTO public.counters(id, value) VALUES (gen_random_uuid (), 0);")
+		pgClient.Exec(context.Background(), "INSERT INTO public.counters(id, value) VALUES (gen_random_uuid(), 0);")
 	}
 }
 
 func GetCounter() (int64, error) {
-	value, redisError := client.Get(counterKey).Result()
+	value, redisError := redisClient.Get(REDIS_COUNTER_KEY).Result()
 	fmt.Printf("Cached Value %q\n", value)
 	res, parseError := strconv.ParseInt(value, 10, 64)
 	if redisError != nil || parseError != nil {
 		res = getFreshCounter()
 		fmt.Printf("Fresh counter %v\n", res)
-		client.Set(counterKey, res, expirySeconds)
+		redisClient.Set(REDIS_COUNTER_KEY, res, REDIS_COUNTER_EXPIRY_MILLIS)
 	}
 	return res, nil
 }
@@ -50,7 +52,6 @@ func Increment() error {
 	return nil
 }
 
-// assumes a value will always return from db
 func getFreshCounter() int64 {
 	var value int64
 	rows, err := pgClient.Query(context.Background(), "select value from counters limit 1")
